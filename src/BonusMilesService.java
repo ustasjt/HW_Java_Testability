0f5d5735eaf9e2641e5a17c1112e770c7b24015c
ustas <ustas-jt@yandex.ru>
2023-11-15 20:02:53 +0300

    Application for calculating miles

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int step = 20;
+        int result = (price / step);
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_205;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+    }
+
+}
